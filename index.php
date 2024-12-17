c16521065a6caf6ff322b3970ae50e7f2f68116b
Brezhneva13 <165768374+Brezhneva13@users.noreply.github.com>
2024-12-17 19:45:51 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Добро пожаловать</title>
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+<body>
+    <h1>Добро пожаловать в систему управления транзакциями</h1>
+    <a href="login.php">Войти</a>
+    <a href="register.php">Зарегистрироваться</a>
+</body>
+</html>
